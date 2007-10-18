724f0626452af17291e999dc7a4c852200decae2
Duncan Coutts <duncan@haskell.org>
2007-10-18 11:07:24 +0000

    Replace 20 lines of Setup.hs code with 2 lines of configuration in .cabal file It's conditionaly building/exposing the System.Process module, only with ghc. It was being done previously in Setup.hs with an egregious hack. Conditionally exposing modules is pretty unpleasent, but at least it's now clear that's what it is doing.

--- Setup.hs
+++ Setup.hs
@@ -1,36 +1,6 @@
 module Main (main) where
 
-import Data.List
 import Distribution.Simple
-import Distribution.PackageDescription
-import Distribution.Simple.LocalBuildInfo
 
 main :: IO ()
-main = do 
-          let hooks = defaultUserHooks {
-                  buildHook = filter_modules_hook
-                            $ buildHook defaultUserHooks,
-                  makefileHook = filter_modules_hook
-                               $ makefileHook defaultUserHooks,
-                  instHook = filter_modules_hook
-                           $ instHook defaultUserHooks }
-          defaultMainWithHooks hooks
-
-type Hook a = PackageDescription -> LocalBuildInfo -> UserHooks -> a -> IO ()
-
-filter_modules_hook :: Hook a -> Hook a
-filter_modules_hook f pd lbi uhs x
- = let build_filter = case compilerFlavor $ compiler lbi of
-                          GHC -> const True
-                          _ -> isPortableBuild
-       lib' = case library pd of
-                  Just lib ->
-                      let ems = filter build_filter (exposedModules lib)
-                      in lib { exposedModules = ems }
-                  Nothing -> error "Expected a library"
-       pd' = pd { library = Just lib' }
-   in f pd' lbi uhs x
-
-isPortableBuild :: String -> Bool
-isPortableBuild "System.Process" = False
-isPortableBuild _ = True
+main = defaultMainWithHooks defaultUserHooks
