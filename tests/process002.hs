a49b60e854f2558186bfd6aac668938be8f0c8eb
Simon Marlow <simonmarhaskell@gmail.com>
2008-04-28 22:53:05 +0000

    move process tests here from the main testsuite

---
+++ tests/process002.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS -cpp #-}
+import System.Process
+import System.IO
+
+#ifdef mingw32_HOST_OS
+cmd = "ls"
+#else
+cmd = "/bin/ls"
+#endif
+
+main = do
+  h <- openFile "output" WriteMode
+  ph <- runProcess cmd [] Nothing Nothing Nothing (Just h) (Just h)
+  waitForProcess ph
+  return ()
