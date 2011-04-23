192b791dea93e8727713ab33a397aab4e7ffdc71
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2011-04-24 03:47:25 +1200

    Win32 fixes : process.cabal and test 3994 (win32 process must be started before it can be interrupted)

--- tests/3994.hs
+++ tests/3994.hs
@@ -5,15 +5,18 @@ import System.IO
 import System.Process
 
 main :: IO ()
-main = do (_,_,_,p) <- createProcess (proc "sleep" ["10"])
+main = do (_,Just hout,_,p) <- createProcess (proc "sh" ["-c", "echo start; sleep 10"])
                             { std_out = CreatePipe, create_group = True }
+          start <- hGetLine hout
+          putStrLn start
           interruptProcessGroupOf p
           t <- myThreadId
                -- timeout
           forkIO $ do
             threadDelay 5000000
+            putStrLn "Interrupting a Running Process Failed"
+            hFlush stdout
             killThread t
           waitForProcess p
           putStrLn "end"
           return ()
-
